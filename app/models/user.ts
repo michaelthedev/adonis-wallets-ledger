d2542204396ff83c4813e8d33e1871a7f72c73a3
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2026-09-12 21:00:04 +0100

    fix initials & accessor for fullName in User model

--- app/models/user.ts
+++ app/models/user.ts
@@ -9,10 +9,10 @@ export default class User extends compose(UserSchema, withAuthFinder(hash)) {
   declare currentAccessToken?: AccessToken
 
   get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
+    return `${this.firstName.charAt(0)}${this.lastName.charAt(0)}`.toUpperCase();
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
