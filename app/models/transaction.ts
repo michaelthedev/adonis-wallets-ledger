7a3ae777fc1ae7fb91a883d81ffbb01f4f81c742
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2026-09-16 10:33:24 +0100

    transaction beforeCreate hook to fill uid

--- app/models/transaction.ts
+++ app/models/transaction.ts
@@ -1,9 +1,15 @@
 import { TransactionSchema } from '#database/schema'
-import {hasMany} from "@adonisjs/lucid/orm";
+import {beforeCreate, hasMany} from "@adonisjs/lucid/orm";
 import type {HasMany} from "@adonisjs/lucid/types/relations";
 import LedgerEntry from "#models/ledger_entry";
+import {DateTime} from "luxon";
 
 export default class Transaction extends TransactionSchema {
+  @beforeCreate()
+  static assignUid(transaction: Transaction) {
+    transaction.uid ??= DateTime.now().toFormat('yymmddHHss')+Math.floor(Math.random() * 10000).toString().padStart(4, '0')
+  }
+
   @hasMany(() => LedgerEntry)
   declare ledgerEntries: HasMany<typeof LedgerEntry>
 }
