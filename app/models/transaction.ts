e737b01ed689fed64e34767cccec6b583e942ca8
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2026-09-18 10:59:08 +0100

    fix: MM for months instead of minutes

--- app/models/transaction.ts
+++ app/models/transaction.ts
@@ -7,7 +7,7 @@ import {DateTime} from "luxon";
 export default class Transaction extends TransactionSchema {
   @beforeCreate()
   static assignUid(transaction: Transaction) {
-    transaction.uid ??= DateTime.now().toFormat('yymmddHHss')+Math.floor(Math.random() * 10000).toString().padStart(4, '0')
+    transaction.uid ??= DateTime.now().toFormat('yyMMddHHss')+Math.floor(Math.random() * 10000).toString().padStart(4, '0')
   }
 
   @hasMany(() => LedgerEntry)
