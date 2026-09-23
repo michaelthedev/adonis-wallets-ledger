0c5796b3e5e466423d972c0004884a659fc733b6
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2026-09-23 13:06:40 +0100

    more test cases for deposit
    - deposit fails on invalid amount
    - deposit fails on invalid currency
    - deposit fails on missing token

--- tests/functional/wallets/deposit.spec.ts
+++ tests/functional/wallets/deposit.spec.ts
@@ -1,10 +1,9 @@
 import { test } from '@japa/runner'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { createUser, createWallet } from '#tests/helpers/index'
+import LedgerEntry from '#models/ledger_entry'
 
-test.group('Wallets -> deposit', (group) => {
-  group.each.setup(() => testUtils.db().truncate())
-
+test.group('Wallets -> deposit', () => {
   test('deposit fund into wallet', async ({ client, assert }) => {
     const user = await createUser()
     const wallet = await createWallet(user, 'USD')
@@ -21,5 +20,53 @@ test.group('Wallets -> deposit', (group) => {
 
     const balance = await wallet.balance()
     assert.equal(balance, 500)
+
+    const depositEntry = await LedgerEntry.query()
+      .where('wallet_id', wallet.id)
+      .where('direction', 'credit')
+      .first()
+
+    assert.isNotNull(depositEntry)
+    assert.equal(depositEntry?.amount, 500)
+  })
+
+  test('deposit fails on invalid amount', async ({ client }) => {
+    const user = await createUser()
+
+    const response = await client
+      .visit('wallets.deposit')
+      .loginAs(user)
+      .json({
+        amount: -1,
+        currency: 'USD'
+      })
+
+    response.assertStatus(422)
+    // response.assertBodyContains();
+  })
+
+  test('deposit fails on invalid currency', async ({ client }) => {
+    const user = await createUser()
+
+    const response = await client
+      .visit('wallets.deposit')
+      .loginAs(user)
+      .json({
+        amount: 10,
+        currency: 'XYZ'
+      })
+
+    response.assertStatus(422)
+  })
+
+  test('deposit fails on missing token', async ({ client }) => {
+    const response = await client
+      .visit('wallets.deposit')
+      .json({
+        amount: 10,
+        currency: 'USD'
+      })
+
+    response.assertStatus(401)
   })
 })
